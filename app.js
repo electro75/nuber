718937a65d795e0da79df1829b58db07932a37aa
sahil n <sahil@mutterfly.in>
2018-07-04 11:45:14 +0530

    adds connection to a separate database for test env and also a test helper file.

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const app = express();
 const routes = require('./routes/routes');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nuber');
+
+if (process.env.NODE_ENV !== 'test') {
+    mongoose.connect('mongodb://localhost/nuber');
+}
+
 
 app.use(express.json());         // app.use call should always be called before routes call.
 routes(app);                     // body-parser middleware is now inbuilt in express.
